deefb54fefd40126eae7897b399f23c719d266a6
Moses Alcantara <mosesalcantara123@gmail.com>
2024-04-25 11:03:26 +0800

    Added Dashboard Loader

--- listings/public/js/dashboard.js
+++ listings/public/js/dashboard.js
@@ -9,14 +9,33 @@ $(document).ready(function () {
 })
 
 function get_data() {
-    $.ajax({
-        url: `/dashboard/get-data/`,
-        method: "GET",
-        success: function (res) {
-            console.log(res)
-        },
-        error: function (res) {
+    $(".dash-loader").addClass("d-none");
+    var loader = `
+                    <div class="loading-spinner spinner-border text-primary" role="status"></div>
+                `
+    $(loader).insertAfter(".dash-loader");
 
-        },
-    })
+    setInterval(() => {
+        $.ajax({
+            url: `/dashboard/get-data/`,
+            method: "GET",
+            success: function (res) {
+                $(".dash-loader").removeClass("d-none");
+                $(".loading-spinner").addClass("d-none");
+                console.log(res)
+                var counts = res.counts
+    
+                $('#counts-clients').text(counts.clients)
+                $('#counts-coordinators').text(counts.coordinators)
+                $('#counts-agents').text(counts.agents)
+                $('#counts-projects').text(counts.projects)
+                $('#counts-units').text(counts.units)
+            },
+            error: function (res) {
+    
+            },
+        })
+    }, 3000)
+
+
 }
