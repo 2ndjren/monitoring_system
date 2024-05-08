22fdf5fa7a0bd269aee2497d7893cc6714e37226
Jared Baren <jpren2000@gmail.com>
2024-05-08 13:10:17 +0800

    push

--- public/js/contracts.js
+++ public/js/contracts.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
         $("#addForm span").remove();
 
         $.ajax({
-            url: `/${ent}/add/`,
+            url: `/${ent}/add`,
             method: "POST",
             data: new FormData(this),
             contentType: false,
@@ -65,7 +65,7 @@ $(document).ready(function () {
 
         $.ajax({
             type: "POST",
-            url: `/${ent}/upd/`,
+            url: `/${ent}/upd`,
             data: new FormData(this),
             contentType: false,
             processData: false,
@@ -104,7 +104,7 @@ $(document).ready(function () {
         e.preventDefault();
         $.ajax({
             type: "POST",
-            url: `/${ent}/del/`,
+            url: `/${ent}/del`,
             data: $(this).serialize(),
             success: function (res) {
                 showtoastMessage("text-success", "Delete Successful", res.msg);
@@ -141,7 +141,7 @@ $(document).ready(function () {
 
         $.ajax({
             method: "POST",
-            url: `/${ent}/edit/`,
+            url: `/${ent}/edit`,
             data: { id: id },
             success: function (res) {
                 var record = res.record;
