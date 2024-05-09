583e562370bddbf1e9483aaccd986d0df0deeaf4
Moses Alcantara <mosesalcantara123@gmail.com>
2024-05-09 09:14:36 +0800

    update dashboard

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -134,6 +134,6 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    {{-- <script src="{{ asset('js/dashboard.js') }}"></script> --}}
+    <script src="{{ asset('js/dashboard.js') }}"></script>
 
 @endsection
